629201f2c47ee3ac4a774a92d5f4675dedfaa6f8
Tebo21 <pasante3@gigacomputers.com.ec>
2021-06-01 23:04:36 -0500

    Cambios en la tabla persona Discapacidad persona Boolean a String

--- src/main/java/com/app/ista/model/Persona.java
+++ src/main/java/com/app/ista/model/Persona.java
@@ -18,118 +18,132 @@ public class Persona {
 	private String fechaNacimiento;
 	private int edad;
 	private String nacionalidad;
-	private String discapacidad;
+	private boolean discapacidad;
 	private String estado_civil;
 	
-	public Persona(String cedula, String nombres, String apellidos, String direccion, String celular, String correo,
-			String genero, String fechaNacimiento, int edad, String nacionalidad, String discapacidad, String estado_civil) {
-		this.cedula = cedula;
-		this.nombres = nombres;
-		this.apellidos = apellidos;
-		this.direccion = direccion;
-		this.celular = celular;
-		this.correo = correo;
-		this.genero = genero;
-		this.fechaNacimiento = fechaNacimiento;
-		this.edad = edad;
-		this.nacionalidad=nacionalidad;
-		this.discapacidad=discapacidad;
-		this.estado_civil=estado_civil;
+
+
+	public Persona() {
 	}
+
 
 	public String getCedula() {
 		return cedula;
 	}
 
+
 	public void setCedula(String cedula) {
 		this.cedula = cedula;
 	}
+
 
 	public String getNombres() {
 		return nombres;
 	}
 
+
 	public void setNombres(String nombres) {
 		this.nombres = nombres;
 	}
+
 
 	public String getApellidos() {
 		return apellidos;
 	}
 
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+
 
 	public String getDireccion() {
 		return direccion;
 	}
 
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 
 	public String getCelular() {
 		return celular;
 	}
 
+
 	public void setCelular(String celular) {
 		this.celular = celular;
 	}
+
 
 	public String getCorreo() {
 		return correo;
 	}
 
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
+
 
 	public String getGenero() {
 		return genero;
 	}
 
+
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
+
 
 	public String getFechaNacimiento() {
 		return fechaNacimiento;
 	}
 
+
 	public void setFechaNacimiento(String fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
+
 
 	public int getEdad() {
 		return edad;
 	}
 
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
 
-	public String getnacionalidad(){
+
+	public String getNacionalidad() {
 		return nacionalidad;
 	}
 
-	public void setNacionalidad(String nacionalidad){
-		this.nacionalidad=nacionalidad;
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
 	}
 
-	public String getdiscapacidad(){
+
+	public boolean isDiscapacidad() {
 		return discapacidad;
 	}
 
-	public void setDiscapacidad(String nacionalidad){
-		this.nacionalidad=nacionalidad;
+
+	public void setDiscapacidad(boolean discapacidad) {
+		this.discapacidad = discapacidad;
 	}
 
-	public String getestado_civil(){
+
+	public String getEstado_civil() {
 		return estado_civil;
 	}
 
-	public void setEstadoCivil(String estado_civil){
-		this.estado_civil=estado_civil;
+	public void setEstado_civil(String estado_civil) {
+		this.estado_civil = estado_civil;
 	}
+
+	
 }
