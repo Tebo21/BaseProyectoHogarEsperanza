ca397f464899a9516a18124169a7d20d0c2f8843
Alvaro-afk-25 <alvaro.guaman.est@tecazuay.edu.ec>
2021-06-12 01:21:52 -0500

    Update ActividadPersonaService.java
    Co-Authored-By: jessi1998 <62186669+jessi1998@users.noreply.github.com>

--- src/main/java/com/app/ista/service/ActividadPersonaService.java
+++ src/main/java/com/app/ista/service/ActividadPersonaService.java
@@ -1,13 +1,11 @@
 package com.app.ista.service;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.app.ista.model.ActividadPersona;
-import com.app.ista.model.Persona;
 import com.app.ista.repository.ActividadPersonaRepository;
 
 @Service
