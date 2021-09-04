25bdec6056847268ab39a6429cff3211f8c1f7c0
andresGuaman <andres.guaman.est@tecazuay.edu.ec>
2021-09-04 05:32:56 -1200

    documentos local server
    documentos local server

--- src/main/java/com/app/ista/controller/DocumentosBeneficiarioController.java
+++ src/main/java/com/app/ista/controller/DocumentosBeneficiarioController.java
@@ -1,12 +1,28 @@
 package com.app.ista.controller;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static java.nio.file.Files.copy;
+import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
+import static java.nio.file.Paths.get;
 import java.util.List;
 
 import com.app.ista.model.DocumentosBeneficiario;
 import com.app.ista.service.DocumentosBeneficiarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +30,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/documentosBeneficiarios")
@@ -22,6 +40,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class DocumentosBeneficiarioController {
     @Autowired
     DocumentosBeneficiarioService documentosBeneficiarioService;
+
+    List<String> files = new ArrayList<String>();
+	private final Path rootLocation = Paths.get("_Path_To_Save_The_File");
 
     @PostMapping(value = "/addDocumentos")
     public ResponseEntity<String> guardarDocumentoBeneficiaros(@RequestBody DocumentosBeneficiario documentosBeneficiario){
@@ -43,4 +64,30 @@ public class DocumentosBeneficiarioController {
         documentosBeneficiarioService.guardardoc(documentosBeneficiario2);
         return documentosBeneficiario2;
     }
+    public static final String DIRECTORY = System.getProperty("user.home") + "/Downloads/";
+    @PostMapping("/upload")
+    public ResponseEntity<List<String>> uploadFiles(@RequestParam("files")List<MultipartFile> multipartFiles) throws IOException {
+        List<String> filenames = new ArrayList<>();
+        for(MultipartFile file : multipartFiles) {
+            String filename = StringUtils.cleanPath(file.getOriginalFilename());
+            Path fileStorage = get(DIRECTORY, filename).toAbsolutePath().normalize();
+            copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
+            filenames.add(filename);
+        }
+        return ResponseEntity.ok().body(filenames);
+    }
+    @GetMapping("download/{filename}")
+    public ResponseEntity<Resource> downloadFiles(@PathVariable("filename") String filename) throws IOException {
+        Path filePath = get(DIRECTORY).toAbsolutePath().normalize().resolve(filename);
+        if(!Files.exists(filePath)) {
+            throw new FileNotFoundException(filename + " was not found on the server");
+        }
+        Resource resource = new UrlResource(filePath.toUri());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("File-Name", filename);
+        httpHeaders.add(CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename());
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+                .headers(httpHeaders).body(resource);
+    }
+
 }
